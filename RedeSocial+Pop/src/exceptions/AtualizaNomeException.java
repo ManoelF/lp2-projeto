1551143b7fd64a10b18445164cddcd0e2ba81370
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2015-08-02 14:30:34 -0300

    preparando uml

---
+++ RedeSocial+Pop/src/exceptions/AtualizaNomeException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class AtualizaNomeException extends AtualizaPerfilException {
+
+	public AtualizaNomeException() {
+		super(" Nome dx usuarix nao pode ser vazio.");
+	}
+	
+}
