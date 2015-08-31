e799f79ee1c4a1f5f558b92bfdcec120b67bae6a
ManoelF <jose.ferreira@ccc.ufcg.edu.br>
2015-08-31 11:11:11 -0300

    Teste deque

--- RedeSocial+Pop/src/testes/Datastest.java
+++ RedeSocial+Pop/src/testes/Datastest.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.*;
 import java.text.ParseException;
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
 
 import logica.Usuario;
 import logica.Util;
@@ -20,6 +23,19 @@ public class Datastest {
 	public void test() {
 		try {
 			Usuario user = new Usuario("ze", "ze@email.com", "1213", "10/05/1990");
+			Deque<Usuario> dequeA = new LinkedList<>();
+			Deque<String> dequeB = new ArrayDeque<>();
+			
+			/*dequeB.add("Bola");
+			dequeB.add("Faca");
+			dequeB.add("bila");
+			dequeB.add("barra");
+			dequeB.add("marte");
+			dequeB.add("java");
+			dequeB.add("python");*/
+			System.out.println(dequeB.pollLast());
+			System.out.println(dequeB);
+			
 		} catch (CadastroInvalidoException e) {
 			System.out.println(e.getMessage());
 		}
