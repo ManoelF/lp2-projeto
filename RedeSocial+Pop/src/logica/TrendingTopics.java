8512061bb085131ad20899f4766a6b41d203366d
ManoelF <jose.ferreira@ccc.ufcg.edu.br>
2015-09-26 00:53:31 -0300

    Criacao da entidade TrendingTopics

---
+++ RedeSocial+Pop/src/logica/TrendingTopics.java
@@ -0,0 +1,20 @@
+package logica;
+
+public class TrendingTopics {
+
+	
+	/*
+	 * Aqui será implementado a classe com a função de criar o trending topic, ou seja, o ranking das tres hashtags mais usadas
+	 * 
+	 * Inicialmente pensei em um contrutor que receberia a lista de usuários cadastrados e apartir dela fazer toda a buscas das
+	 * hashtags usadas nos posts dos usuarios.
+	 * 
+	 * Estrutura para armazenar os dados, HashMap onde irá mapear hashtag-quantidade, que vai pegar todos os posts
+	 * do usuario e adicionar a hashtag no mapa, e a medida que for iterando fará a verificação se essa hashtag está no
+	 * dicionari se sim incrementa mais um na quantidade, se não acrescenta a hashtage e um na quantidade.
+	 * 
+	 * Por fim, fara uma busca procurando as hashtags que tem a maior quantidade. Tendo essa informação, usaremos o string Buffer
+	 * para fazer a concatenação no formato desejado.
+	 * 
+	 */
+}
