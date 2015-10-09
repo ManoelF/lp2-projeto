9ae089b10966af27e00d85fa89d6c348bf6c2d5d
ManoelF <jose.ferreira@ccc.ufcg.edu.br>
2015-10-08 22:14:31 -0300

    Rankin atualizado em master

--- RedeSocial+Pop/src/logica/Ranking.java
+++ RedeSocial+Pop/src/logica/Ranking.java
@@ -4,13 +4,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
-<<<<<<< HEAD
-=======
-
-import logica.Usuario;
-import logica.Controller;
-import logica.ordenacao.OrdenaDecrescente;
->>>>>>> c65b2decb0436c6e39e0de435247fe4471b5f8ee
 
 import logica.Usuario;
 import logica.ordenacao.OrdenaDecrescente;
@@ -40,31 +33,18 @@ public class Ranking{
 	}
 	
 	public void rankingUsuario(){
-<<<<<<< HEAD
-		ordenaCrescente(usuarios);
-=======
 		
 	}
 
 
 	public void atualizaRanking() {
 ordenaCrescente(usuarios);
->>>>>>> c65b2decb0436c6e39e0de435247fe4471b5f8ee
 		
 		int cont = 0;
 		for (int i = 0; i < usuarios.size(); i++) {
 			if(usuarios.get(i) != null){
 				comMaisPop.add(usuarios.get(i));
 				cont++;
-<<<<<<< HEAD
-			}
-			if(cont == 3){
-				break;
-			}
-		}
-		
-		if(usuarios.size() > 3){
-=======
 			}
 			if(cont == 3){
 				break;
@@ -73,7 +53,6 @@ ordenaCrescente(usuarios);
 		
 		if(usuarios.size() > TAMANHO_MAXIMO){
 			ordenaCrescente(usuarios);
->>>>>>> c65b2decb0436c6e39e0de435247fe4471b5f8ee
 			ordenaDecrescente(usuarios);
 			
 			int contador = 0;
@@ -84,29 +63,6 @@ ordenaCrescente(usuarios);
 				}
 				if(contador == 3){
 					break;
-<<<<<<< HEAD
-				}
-			}
-		}
-	}
-	
-	
-	public String toString(){
-		String imprime = "";
-		
-		for(int i = 0; i < comMaisPop.size(); i++){
-			imprime += comMaisPop.get(i);
-		}
-	
-		for(int i = 0; i < comMenosPop.size(); i++){
-			imprime += comMenosPop.get(i);
-			}
-		
-		return imprime;
-		
-	}
-			
-=======
 				}else{
 					System.out.println("o tamanho da lista eh menor do que 3");
 				}
@@ -140,6 +96,5 @@ ordenaCrescente(usuarios);
 		
 	}
 		
->>>>>>> c65b2decb0436c6e39e0de435247fe4471b5f8ee
 	
 } // fecha a classe ranking
